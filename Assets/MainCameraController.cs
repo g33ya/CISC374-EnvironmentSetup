2e810462f2b612b39cb694f2a1519f9d40e006cb
g33ya <gscozzaro2004@gmail.com>
2025-02-09 19:28:58 -0500

    Logging a CS joke

--- Assets/MainCameraController.cs
+++ Assets/MainCameraController.cs
@@ -5,7 +5,7 @@ public class MainCameraController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World");
+        Debug.Log("What's the best way to debug? With a fly swatter! *laugh here*");
     }
 
     // Update is called once per frame
